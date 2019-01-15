d189fa60edf914301a5b6dae1c5f48ca534db811
Jeffrey Chen <jeffreychen2016@gmail.com>
2019-01-14 21:06:45 -0600

    added customer controller and customerAccess. Added method to post customer data along with firebase uid into customer table after registration succeed

--- bangazon2/DataAccess/CustomerAccess.cs
+++ bangazon2/DataAccess/CustomerAccess.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using bangazon2.Models;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +11,24 @@ namespace bangazon2.DataAccess
 {
     public class CustomerAccess
     {
+        private string ConnectionString;
+
+        public CustomerAccess(IConfiguration config)
+        {
+            ConnectionString = config.GetSection("ConnectionString").Value;
+        }
+
+        public bool AddCustomer(Customer customer)
+        {
+            using(var dbConnection = new SqlConnection(ConnectionString))
+            {
+                dbConnection.Open();
+
+                var result = dbConnection.Execute(@"INSERT INTO Customer (FirstName,LastName,IsActive,FirebaseId) 
+                                                    VALUES (@FirstName,@LastName,@IsActive,@FirebaseId);", customer);
+
+                return result == 1;
+            }
+        }
     }
 }

79e1d048f858ccf550962f3956fb691d7d084636
Tom Dugan <thomas@aadwebb.com>
2019-01-14 20:09:54 -0600

    start backend

---
+++ bangazon2/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bangazon2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : Controller
+    {
+        private readonly 
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ bangazon2/DataAccess/ProductStorage.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using bangazon2.Models;
+
+namespace bangazon2.DataAccess
+{
+    public class ProductStorage
+    {
+        private string ConnectionString;
+
+        public ProductStorage(IConfiguration config)
+        {
+            ConnectionString = config.GetSection("ConnectionString").Value;
+        }
+
+        // Get Products
+        public IEnumerable<ProductsForGet> GetAllProducts()
+        {
+            using (var db = new SqlConnection(ConnectionString))
+        }
+    }
+}

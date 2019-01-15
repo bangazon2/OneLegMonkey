84e1efedb88f36bbc0efd19a60f2027fa1cd7991
Tom Dugan <thomas@aadwebb.com>
2019-01-14 19:54:36 -0600

    frontend done for now - reads and displays products

---
+++ bangazon2/ClientApp/src/DBRequests/productRequest.js
@@ -0,0 +1,18 @@
+﻿import axios from 'axios';
+
+const getRequest = () => {
+    return new Promise((resolve, reject) => {
+        axios
+            .get(`/api/product/products`)
+            .then(results => {
+                resolve(results.data);
+            })
+            .catch(error => {
+                reject(error);
+            })
+    })
+}
+
+export default {
+    getRequest,
+}

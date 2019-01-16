87bb9e665eed9d8ca3bc30d5015a56b5da75ceb1
Mary Alice Orr <m.a.orr17@gmail.com>
2019-01-15 21:28:53 -0600

    got my get working and wrote my sql for

---
+++ bangazon2/DataAccess/ProductTypeStorage.cs
@@ -0,0 +1,33 @@
+﻿using bangazon2.Models;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bangazon2.DataAccess
+{
+    public class ProductTypeStorage
+    {
+        private readonly string ConnectionString;
+
+        public ProductTypeStorage(IConfiguration config)
+        {
+            ConnectionString = config.GetSection("ConnectionString").Value;
+        }
+
+        public List<ProductType> GetAllProductTypes()
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                var result = connection.Query<ProductType>(@"SELECT * FROM ProductType");
+
+                return result.ToList();
+            }
+        }
+    }
+}
